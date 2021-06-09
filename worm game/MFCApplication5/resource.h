f33e62c0eee3da5479871e1a3dc8e1dc0bf32e61
SSong <sonk55@naver.com>
2021-06-10 00:19:08 +0900

    Create resource.h

---
+++ worm game/MFCApplication5/resource.h
@@ -0,0 +1,23 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication5.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_MFCApplication5TYPE         130
+#define IDB_BITMAP1                     310
+#define IDB_BITMAP2                     313
+#define IDB_BITMAP3                     314
+#define IDB_BITMAP4                     315
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        316
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
